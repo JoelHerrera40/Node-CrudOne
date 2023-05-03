8641b1ee0e04ff072e2188102530ca42a5c68bf6
JOEL ARMANDO HERRERA CUEVAS <joel.herrera@ikeasi.com>
2023-05-03 14:18:25 -0400

    Structure Created

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.listen(5000, ()=> {
+    console.log('Server corriendo en http://localhost:5000');
+});
