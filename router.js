a0de82ba43fe7eb780862cf34dc82b95478b8666
JOEL ARMANDO HERRERA CUEVAS <joel.herrera@ikeasi.com>
2023-05-03 15:27:45 -0400

    Template Created

--- router.js
+++ router.js
@@ -4,12 +4,16 @@ const router = express.Router();
 const conn= require ('./modelo/db');
 
 router.get('/', (req, res,)=>{
-   conn.query('SELECT * FROM users', (error, results)=>{
-    if (error) {
-        throw error;
-   }else{ res.send(results);
-   }
-})
+    res.render('index', {var1:'Esto es una variable'});
+
+
+
+//    conn.query('SELECT * FROM users', (error, results)=>{
+//     if (error) {
+//         throw error;
+//    }else{ res.send(results);
+//    }
+// })
 });
 
 module.exports= router;
